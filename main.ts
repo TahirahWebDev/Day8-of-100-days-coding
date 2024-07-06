96b3e34b3900e63ec9e039e02f6746d4902510ad
Tahirah Roohi <157677736+TahirahWebDev@users.noreply.github.com>
2024-07-06 01:19:33 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,47 @@
+// Question 22
+
+let friends: string[] = ["Alice", "Bob", "Charlie"];
+console.log(friends[3]);
+friends[2] = "Charlie"; 
+
+
+// Question 23
+
+let car = "subaru";
+
+console.log("Is car == 'subaru'? I predict true");
+console.log(car == "subaru");
+
+console.log("Is car == 'toyota'? I predict false");
+console.log(car == "toyota");
+
+
+// Question 24
+
+// Equality with strings
+console.log("Testing equality with strings:");
+console.log("apple" == "apple"); // True
+// console.log("apple" == "Apple"); // False
+
+// Using the lower case function
+console.log("Testing with lower case:");
+console.log("Apple".toLowerCase() == "apple"); // True
+
+// Numerical tests
+console.log("Numerical tests:");
+console.log(10 > 5); // True
+console.log(2 < 1); // False
+
+// Tests using "and" and "or" operators
+console.log("Tests with 'and' and 'or':");
+console.log(true && false); // False
+console.log(true || false); // True
+
+// Test whether an item is in a array
+let fruits = ["apple", "banana", "cherry"];
+console.log("Is 'apple' in fruits?");
+console.log(fruits.includes("apple")); // True
+
+// Test whether an item is not in a array
+console.log("Is 'mango' not in fruits?");
+console.log(!fruits.includes("mango"));
